7c91f081ee870314ba9f46a7d2ae485ee077a231
Patri <pattymatos.m@gmail.com>
2024-02-20 23:09:06 +0100

    REQ 1 y PU

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -19,9 +19,9 @@ public class ModeloDatosTest {
         System.out.println("Prueba de actualizarJugador");
         String nombre = "Llull";
         ModeloDatos instance = new ModeloDatos();
-        instance.abrirConexionTest();
         instance.insertTestData();
         int before = instance.getJugador(nombre);
+        System.out.println("Before" + before);
         instance.actualizarJugador(nombre);
         int after = instance.getJugador(nombre);
         assertEquals(before + 1, after);
