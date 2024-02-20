bc2824f4808a481bbbd36bbc4a44c00715697fb5
Patri <pattymatos.m@gmail.com>
2024-02-20 22:49:28 +0100

    REQ 1 y PU

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -1,29 +1,44 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.*;
+
 public class ModeloDatosTest {
+
+    private ModeloDatos instance;
+
+    @BeforeAll
+    public void setUp() throws Exception {
+
+        instance = new ModeloDatos();
+        instance.abrirConexion();
+
+    }
+
+    @AfterAll
+    public void destroy() throws Exception {
+
+        instance.cerrarConexion();
+
+    }
 
     @Test
     public void testExisteJugador() {
         System.out.println("Prueba de existeJugador");
         String nombre = "";
-        ModeloDatos instance = new ModeloDatos();
         boolean expResult = false;
         boolean result = instance.existeJugador(nombre);
         assertEquals(expResult, result);
-        //fail("Fallo forzado.");
+        // fail("Fallo forzado.");
     }
 
     @Test
     public void testActualizarJugador() {
         System.out.println("Prueba de actualizarJugador");
         String nombre = "Llull";
-        ModeloDatos instance = new ModeloDatos();
-        instance.abrirConexion();
         int before = instance.getJugador(nombre);
         instance.actualizarJugador(nombre);
         int after = instance.getJugador(nombre);
-        instance.cerrarConexion();
         assertEquals(before + 1, after);
     }
 }
