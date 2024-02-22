ed5f4019f73ed1a631ecee7761a9676dedc12618
Patri <pattymatos.m@gmail.com>
2024-02-22 19:14:55 +0100

    req 2

--- src/main/java/ModeloDatos.java
+++ src/main/java/ModeloDatos.java
@@ -64,8 +64,9 @@ public class ModeloDatos {
 
     public String getData() {
 
-        String j = "aaa";
+        String j = "aaa ";
         try {
+            j = j.concat("buenas");
             set = con.createStatement();
             rs = set.executeQuery("SELECT nombre, votos FROM Jugadores");
             while (rs.next()) {
